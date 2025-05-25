3e315a461aca89bf0ea2ba1cb89792270f116e22
System User <king.r.paul@gmail.com>
2025-05-25 14:10:50 -0500

    regroup images and css

--- python/make_pix_html.py
+++ python/make_pix_html.py
@@ -127,7 +127,7 @@ HEAD = \
   <meta name="author" content="Paul King">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="shortcut icon" href="favicon.ico" />
+  <link rel="shortcut icon" href="favicon/favicon.ico" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
